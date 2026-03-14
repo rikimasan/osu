13fc293e0c323cb4a353236329597187c8e6603f
Kian Masri <kianmasri@gmail.com>
2026-03-13 21:38:17 -0400

    Add empty OsuRhythmDifficultyPreprocessor scaffold

---
+++ osu.Game.Rulesets.Osu/Difficulty/Preprocessing/Rhythm/OsuRhythmDifficultyPreprocessor.cs
@@ -0,0 +1,15 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Collections.Generic;
+using osu.Game.Rulesets.Difficulty.Preprocessing;
+
+namespace osu.Game.Rulesets.Osu.Difficulty.Preprocessing.Rhythm
+{
+    public static class OsuRhythmDifficultyPreprocessor
+    {
+        public static void ProcessAndAssign(List<DifficultyHitObject> objects, OsuDifficultyConstants tuning)
+        {
+        }
+    }
+}
